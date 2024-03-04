92c864369b2839a4eb4129538115a62a92d9df65
saachins-aws <saachins@amazon>
2024-03-04 16:30:15 +0530

    main commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+   region = "us-west-2"
+}
+
+resource "aws_instance" "ex_instance" {
+    ami = var.ami-id
+    instance_type = "t2.micro"
+    tags = {
+      Name = "Equifax-instance"
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "ami-id"{
+    description = " The ID of the AMI to use for the EC2 instance"
+    type = string
+    
+}
