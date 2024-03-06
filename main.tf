d3a57748631ab042755fd3b577269368af2cc172
saachins-aws <saachins@amazon>
2024-03-06 09:53:07 +0530

    change variable

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-   region = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "ex_instance" {
-    ami = var.ami-id
-    instance_type = "t2.micro"
-    tags = {
-      Name = "Equifax-instance"
-    }
+  ami           = var.ami-id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Equifax-instance"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,8 @@
-variable "ami-id"{
-    description = " The ID of the AMI to use for the EC2 instance"
-    type = string
-    
+variable "ami-id" {
+  description = " The ID of the AMI to use for the EC2 instance"
+  type        = string
+
+}
+variable "env" {
+  type = string
 }
