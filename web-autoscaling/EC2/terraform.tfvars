a7f81d88ea37e9d0a44904d14d486d9e20d6e262
saachins-aws <saachins@amazon>
2024-03-18 11:57:53 +0530

    first commit

---
+++ web-autoscaling/EC2/terraform.tfvars
@@ -0,0 +1,2 @@
+ami-id = "ami-0440d3b780d96b29d"
+env    = "testing"
---
+++ web-autoscaling/EC2/variables.tf
@@ -0,0 +1,8 @@
+variable "ami-id" {
+  description = " The ID of the AMI to use for the EC2 instance"
+  type        = string
+
+}
+variable "env" {
+  type = string
+}
